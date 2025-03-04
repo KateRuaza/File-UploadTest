8484f64ceccc7abbaa77bc002b830d6952e69b8e
Kenn <johnkenneth.ornias@gmail.com>
2025-03-04 11:17:06 +0800

    refactor: run laravel pint

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreFileRequest;
-use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
     public function store(StoreFileRequest $request)
     {
-        $path = $request->file('file')->store('uploads');
+        $request->file('file')->store('uploads');
 
         return back()->with('success', 'File uploaded successfully!');
     }
